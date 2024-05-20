e341e9dec77a42f9c722c4c6fb0ec33f200dca4a
Clyde H. Gevero <gevero530@gmail.com>
2024-05-21 01:55:19 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'VortexMedia'
+  base:'/VortexMedia/'
 })
 
 // import { defineConfig } from 'vite'
