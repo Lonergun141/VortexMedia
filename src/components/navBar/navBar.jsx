ac0a9d7b70e713f565e8479e0d38a6e0f5607b44
Clyde H. Gevero <gevero530@gmail.com>
2024-05-21 09:44:19 +0800

    paths

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -21,7 +21,7 @@ const NavigationBar = () => {
       .unwrap()
       .then(() => {
         localStorage.removeItem('accessToken'); 
-        navigate('/');
+        navigate('/VortexMedia/');
       })
       .catch((error) => {
         console.error('Logout error:', error);
