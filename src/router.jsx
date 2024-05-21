ca5002654cc0ca3d43abea58905b32a9238543d8
Clyde H. Gevero <gevero530@gmail.com>
2024-05-21 08:53:26 +0800

    browserrouter

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import SignIn from './pages/authentication/signIn';
 import NewsPage from './pages/newsPage/newsPage';
 import SignUp from './pages/authentication/signUp';
@@ -14,63 +14,61 @@ import Entertainment from './pages/newsPage/entertainment';
 import Profile from './pages/profile/profile';
 import NotFound from './pages/notFound';
 
-export const router = createHashRouter([
-  {
-    path: '/VortexMedia/',
-    element: <SignIn />,
-    children: [
-      {
-        path: '/VortexMedia/SignUp',
-        element: <SignUp />,
-      },
-      {
-        path: '/VortexMedia/NewsPage',
-        element: <NewsPage />,
-      },
-      {
-        path: '/VortexMedia/activate/:uid/:token',
-        element: <ActivateAccount />,
-      },
-      {
-        path: '/VortexMedia/ResetPass',
-        element: <ResetPassword />,
-      },
-      {
-        path: '/VortexMedia/password/reset/confirm/:uid/:token',
-        element: <Resetpasswordchange />,
-      },
-      {
-        path: '/VortexMedia/Home',
-        element: <Home />,
-      },
-      {
-        path: '/VortexMedia/World',
-        element: <World />,
-      },
-      {
-        path: '/VortexMedia/Politics',
-        element: <Politics />,
-      },
-      {
-        path: '/VortexMedia/Sports',
-        element: <Sports />,
-      },
-      {
-        path: '/VortexMedia/Business',
-        element: <Business />,
-      },
-      {
-        path: '/VortexMedia/Entertainment',
-        element: <Entertainment />,
-      },
-      {
-        path: '/VortexMedia/Profile',
-        element: <Profile />,
-      },
-      {
-        path: '*',
-        element: <NotFound />,
-      },
-    ],
-  },
+export const router = createBrowserRouter([
+	{
+		path: '/VortexMedia/',
+		element: <SignIn />,
+	},
+	{
+		path: '/VortexMedia/SignUp',
+		element: <SignUp />,
+	},
+	{
+		path: '/VortexMedia/NewsPage',
+		element: <NewsPage />,
+	},
+	{
+		path: '/VortexMedia/activate/:uid/:token',
+		element: <ActivateAccount />,
+	},
+	{
+		path: '/VortexMedia/ResetPass',
+		element: <ResetPassword />,
+	},
+	{
+		path: '/VortexMedia/password/reset/confirm/:uid/:token',
+		element: <Resetpasswordchange />,
+	},
+	{
+		path: '/VortexMedia/Home',
+		element: <Home />,
+	},
+	{
+		path: '/VortexMedia/World',
+		element: <World />,
+	},
+	{
+		path: '/VortexMedia/Politics',
+		element: <Politics />,
+	},
+	{
+		path: '/VortexMedia/Sports',
+		element: <Sports />,
+	},
+	{
+		path: '/VortexMedia/Business',
+		element: <Business />,
+	},
+	{
+		path: '/VortexMedia/Entertainment',
+		element: <Entertainment />,
+	},
+	{
+		path: '/VortexMedia/Profile',
+		element: <Profile />,
+	},
+	{
+		path: '*',
+		element: <NotFound />,
+	},
 ]);
